d25aba4c205e4f7e1c7a6d627b393398a2ccebdb
Budhi Octaviansyah <budioct@gmail.com>
2023-05-14 22:23:43 +0700

    form request

---
+++ src/main/java/com/tutorial/servlet/FormRequest.java
@@ -0,0 +1,20 @@
+package com.tutorial.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebServlet
+public class FormRequest {
+
+    /**
+     * Form Request
+     * ● Jika user mengirim POST Form Request, kita juga bisa menggunakan method getParameter seperti pada Query Parameter untuk mendapatkan
+     *   informasi data yang dikirim pada Form Request tersebut
+     * ● Artinya getParameter itu secara otomatis akan mengambil data baik itu dari Form Post ataupun Query Param
+     */
+
+    
+
+
+}
